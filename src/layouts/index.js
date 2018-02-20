e5782befa9bdef2f17a8f13241fb6aecbac4d137
Filip Nikolovski <filip.nikol92@gmail.com>
2018-02-20 21:01:11 +0100

    added the styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
+
+import "@fortawesome/fontawesome/styles.css"
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
